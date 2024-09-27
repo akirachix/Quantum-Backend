917d42df7ec1973be9c9d12227523b02ace1d7d7
Loice-Staice <staiceloicea@gmail.com>
2024-09-27 15:37:15 +0300

    made changes on farmers_id

--- farmer/models.py
+++ farmer/models.py
@@ -5,7 +5,7 @@ class Farmer(models.Model):
     farmers_name = models.CharField(max_length= 100)
     farmer_location = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=15, unique=True)
-    farmer_id = models.IntegerField(primary_key=True)
+    farmer_id = models.AutoField(primary_key=True)
     sensor_id = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
